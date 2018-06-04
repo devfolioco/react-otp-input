3956048ff3f3fd2a952a432e162c3c13edd5e2b0
ajayns <ajay.ns@outlook.com>
2018-06-04 15:36:32 +0530

    :sparkles: Handle backspace, arrow keys

--- src/OtpInput.jsx
+++ src/OtpInput.jsx
@@ -1,6 +1,11 @@
 // @flow
 import React, { Component, PureComponent } from 'react';
 
+// keyCode constants
+const BACKSPACE = 8;
+const LEFT_ARROW = 37;
+const RIGHT_ARROW = 39;
+
 type Props = {
   numberOfInputs: number
 }
@@ -18,6 +23,7 @@ class SingleOtpInput extends PureComponent {
       this.input.select();
     }
   }
+
   render() {
     return (
       <div>
@@ -48,7 +54,7 @@ class OtpInput extends Component<Props> {
 
   focusInput = (input: number) => {
     const { numberOfInputs } = this.props;
-    const activeInput = Math.max(Math.min(numberOfInputs-1 , input),0);
+    const activeInput = Math.max(Math.min(numberOfInputs - 1 , input),0);
 
     this.setState({
       activeInput
@@ -56,13 +62,37 @@ class OtpInput extends Component<Props> {
   }
 
   focusNextInput = () => {
-    const { activeInput } = this.state;
-    
+    const { activeInput } = this.state;    
     this.focusInput(activeInput + 1);
+  }
+
+  focusPrevInput = () => {
+    const { activeInput } = this.state;
+    this.focusInput(activeInput - 1)
   }
 
   handleOnChange = (e: Object) => {
     this.focusNextInput();
+  }
+  
+  handleOnKeyDown = (e: Object) => {
+    switch(e.keyCode) {
+      case BACKSPACE:
+        e.preventDefault();
+        e.target.value = '';
+        this.focusPrevInput();
+        break;
+      case LEFT_ARROW:
+        e.preventDefault();
+        this.focusPrevInput();
+        break;
+      case RIGHT_ARROW:
+        e.preventDefault();
+        this.focusNextInput();
+        break;
+      default:
+        break;
+    }
   }
 
   renderInputs = () => {
@@ -76,6 +106,7 @@ class OtpInput extends Component<Props> {
           key={i} 
           focus={activeInput === i}
           onChange={this.handleOnChange}
+          onKeyDown={this.handleOnKeyDown}
           onFocus={e => {
             this.setState({
               activeInput: i,
