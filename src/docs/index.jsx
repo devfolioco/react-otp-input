c01944b671408eec124597d269cfd0fa84bb5142
ajayns <ajay.ns@outlook.com>
2018-06-07 15:27:29 +0530

    :recycle: Linting fixes

--- src/docs/index.jsx
+++ src/docs/index.jsx
@@ -1,27 +1,27 @@
-import React from "react";
-import { render } from "react-dom";
-import OtpInput from "../../lib";
-import "./styles.css";
+import React from 'react';
+import { render } from 'react-dom';
+import OtpInput from '../../lib';
+import './styles.css';
 
 function Demo() {
   return (
     <div>
       <h1>Demo with examples of the component</h1>
       <OtpInput
-          containerStyle={{ padding: '2em' }}
-          inputStyle={{
-            background: 'white',
-            fontSize: 24,
-            color: 'black',
-            border: '1px solid grey',
-            padding: 16,
-          }}
-          numInputs={6}
-          separator={<span style={{ margin: '1em' }}>-</span>}
-          separatorStyle={{ fontWeight: 'bold' }}
-        />
+        containerStyle={{ padding: '2em' }}
+        inputStyle={{
+          background: 'white',
+          fontSize: 24,
+          color: 'black',
+          border: '1px solid grey',
+          padding: 16,
+        }}
+        numInputs={6}
+        separator={<span style={{ margin: '1em' }}>-</span>}
+        separatorStyle={{ fontWeight: 'bold' }}
+      />
     </div>
   );
 }
 
-render(<Demo />, document.getElementById("app"));
+render(<Demo />, document.getElementById('app'));
