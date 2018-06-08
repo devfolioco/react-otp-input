582986411066897a02875bf3f0b5c75b6ff7a7f7
ajayns <ajay.ns@outlook.com>
2018-06-08 14:53:20 +0530

    :sparkles: Demo

--- src/docs/index.jsx
+++ src/docs/index.jsx
@@ -1,27 +1,116 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { render } from 'react-dom';
 import OtpInput from '../../lib';
 import './styles.css';
 
-function Demo() {
-  return (
-    <div>
-      <h1>Demo with examples of the component</h1>
-      <OtpInput
-        containerStyle={{ padding: '2em' }}
-        inputStyle={{
-          background: 'white',
-          fontSize: 24,
-          color: 'black',
-          border: '1px solid grey',
-          padding: 16,
-        }}
-        numInputs={6}
-        separator={<span style={{ margin: '1em' }}>-</span>}
-        separatorStyle={{ fontWeight: 'bold' }}
-      />
-    </div>
-  );
+class Demo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      otp: '',
+      numInputs: 4,
+      separator: '',
+      isDisabled: false,
+    };
+  }
+
+  handleOtpChange = otp => {
+    this.setState({ otp });
+  };
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleCheck = () => {
+    this.setState(prevState => ({ isDisabled: !prevState.isDisabled }));
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state.otp);
+  };
+
+  render() {
+    const { numInputs, separator, isDisabled } = this.state;
+
+    return (
+      <div className="container">
+        <div className="side-bar">
+          <a href="https://github.com/ajayns/react-otp-input" target="_blank">
+            <div className="side-bar__segment side-bar__segment--header">
+              <h2>react-otp-input</h2>
+            </div>
+          </a>
+          <div className="side-bar__segment">
+            <label htmlFor="num-inputs">
+              Number of inputs:
+              <input
+                id="num-inputs"
+                name="numInputs"
+                type="number"
+                value={numInputs}
+                onChange={this.handleChange}
+              />
+            </label>
+          </div>
+          <div className="side-bar__segment">
+            <label htmlFor="separator">
+              Separator
+              <input
+                id="separator"
+                name="separator"
+                type="text"
+                value={separator}
+                onChange={this.handleChange}
+              />
+            </label>
+          </div>
+          <div className="side-bar__segment">
+            <label htmlFor="disabled">
+              <input
+                id="disabled"
+                name="isDisabled"
+                type="checkbox"
+                checked={isDisabled}
+                onChange={this.handleCheck}
+              />
+              Disabled
+            </label>
+          </div>
+          <div className="side-bar__segment side-bar__segment--bottom">
+            <a href="https://github.com/ajayns/react-otp-input">
+              Documentation and Source
+            </a>
+          </div>
+        </div>
+        <div className="view">
+          <div className="card">
+            <form onSubmit={this.handleSubmit}>
+              <h2>Enter verification code</h2>
+              <div className="margin-top--small">
+                <OtpInput
+                  inputStyle={{
+                    width: '3rem',
+                    height: '3rem',
+                    margin: '0 1rem',
+                    fontSize: '2rem',
+                    borderRadius: 4,
+                    border: '1px solid rgba(0,0,0,0.3)',
+                  }}
+                  numInputs={numInputs}
+                  disabled={isDisabled}
+                  onChange={this.handleOtpChange}
+                  separator={<span>{separator}</span>}
+                />
+              </div>
+              <button className="btn margin-top--large">Continue</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 render(<Demo />, document.getElementById('app'));
