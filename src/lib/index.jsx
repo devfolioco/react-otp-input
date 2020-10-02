33b1660d3066340fcca22a118203538414e8e5dc
borntofrappe <borntofrappe@gmail.com>
2020-10-02 20:02:58 +0200

    reword aria-label

--- src/lib/index.jsx
+++ src/lib/index.jsx
@@ -96,7 +96,7 @@ class SingleOtpInput extends PureComponent<*> {
         style={{ display: 'flex', alignItems: 'center' }}
       >
         <input
-          aria-label={`${(index === 0) ? 'Please enter verification code. ' : ''}${isInputNum ? 'Digit' : 'Character'} n° ${index + 1}`}
+          aria-label={`${(index === 0) ? 'Please enter verification code. ' : ''}${isInputNum ? 'Digit' : 'Character'} ${index + 1}`}
           autoComplete="off"
           style={Object.assign(
             { width: '1em', textAlign: 'center' },
