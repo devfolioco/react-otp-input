91c62dfa149d2e0c078cbb202b8fe07a917a2f63
Abhishek Warokar <abhishek11@live.in>
2020-10-31 14:27:02 +0530

    :truck: Move index.d.ts -> typings/index.d.ts

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,16 +1,12 @@
-import React from 'react';
+import * as React from 'react';
 
-export default OtpInput;
-declare class OtpInput extends React.Component {
+export class OtpInput extends React.Component<OtpInputProps, OtpInputState> {
   static defaultProps: {
     numInputs: number;
     onChange: (otp: number) => void;
     isDisabled: boolean;
     shouldAutoFocus: boolean;
     value: string;
-  };
-  state: {
-    activeInput: number;
   };
   getOtpValue: () => any;
   getPlaceholderValue: () => any;
@@ -25,4 +21,28 @@ declare class OtpInput extends React.Component {
   handleOnKeyDown: (e: Object) => void;
   handleOnInput: (e: Object) => void;
   renderInputs: () => any[];
+}
+
+export interface OtpInputProps {
+  className?: string;
+  containerStyle?: Object;
+  disabledStyle?: Object;
+  errorStyle?: Object;
+  focusStyle?: Object;
+  hasErrored?: boolean;
+  inputStyle?: Object;
+  isDisabled?: boolean;
+  isInputNum?: boolean;
+  isInputSecure?: boolean;
+  numInputs: number;
+  onChange: Function;
+  placeholder: string;
+  separator?: Object;
+  shouldAutoFocus?: boolean;
+  value?: string;
+}
+
+export interface OtpInputState {
+  activeInput: number;
+  otp: string[];
 }
